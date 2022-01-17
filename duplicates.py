b8de7007017cca660f70ee7811a65e39411626fb
girishvi <voxrok@gmail.com>
2022-01-17 22:37:12 +0530

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -5,7 +5,7 @@ myList  = [1,2,2,3,4,5,6,7,5,6,7,8,9,10,12,1,23,4,54,4,67,19,10];
 newList = set()
 
 for i in myList:
-    if myList.count(i) >= 2:
+    if myList.count(i) >= 2: #more than 2 occurences, add to new list
         newList.add(i)
 
 print(list(newList))
